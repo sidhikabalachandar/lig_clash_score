c0b935172c35d2e53f22171668ff302c09086303
sidhikabalachandar <sidhikab@stanford.edu>
2021-07-20 15:39:32 -0400

    test_search

--- src/sample/test/test_search.py
+++ src/sample/test/test_search.py
@@ -82,8 +82,6 @@ def main():
     conformer_index = random.choice([i for i in range(len(grouped_conformer_indices))])
 
     pose_file = os.path.join(pose_path, 'exhaustive_search_poses_{}_{}.csv'.format(grid_index, conformer_index))
-    print(pose_file)
-    assert (1 == 2)
 
     if not os.path.exists(pose_file):
         cmd = 'sbatch -p rondror -t 0:20:00 -o {} --wrap="$SCHRODINGER/run python3 search.py group {} {} {} ' \
