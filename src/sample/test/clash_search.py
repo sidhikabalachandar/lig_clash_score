609bd0b04d04e54ca440f827e499590a27ed9494
sidhikabalachandar <sidhikab@stanford.edu>
2021-07-21 15:08:59 -0400

    clash_search

--- src/sample/test/clash_search.py
+++ src/sample/test/clash_search.py
@@ -31,11 +31,19 @@ def main():
     pairs = get_prots(args.docked_prot_file)
     random.shuffle(pairs)
 
+    for residue_cutoff in [1, 2]:
+
+    print()
+    print()
+    print()
+    print()
     print('Simple filter: start clash cutoff is {}'.format(args.start_clash_cutoff))
-    print('Advanced filter: num intolerable residue cutoff is {}'.format(args.residue_cutoff))
+    print('Advanced filter: num intolerable residue cutoff is {}'.format(residue_cutoff))
     print('Correct: rmsd cutoff is {}'.format(args.rmsd_cutoff))
+    print()
 
     for protein, target, start in pairs[:5]:
+        print()
         print(protein, target, start)
         if protein == 'Q86WV6':
             continue
