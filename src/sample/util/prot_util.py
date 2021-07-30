3e87b15b3a3bf003a6fbd344009402b325ea0c08
sidhikabalachandar <sidhikab@stanford.edu>
2021-07-30 16:09:18 -0400

    Search

--- src/sample/util/prot_util.py
+++ src/sample/util/prot_util.py
@@ -175,7 +175,7 @@ def get_grid_size(pair_path, target, start):
     if grid_size % 2 == 1:
         grid_size += 1
 
-    # grid_size = max(grid_size, 6)
+    grid_size = max(grid_size, 6)
     return grid_size
 
 
