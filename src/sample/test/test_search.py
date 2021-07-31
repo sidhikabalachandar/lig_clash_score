45e102a4c443cd42f4aa3352b2ce0fdbaa726f95
sidhikabalachandar <sidhikab@stanford.edu>
2021-07-30 23:24:09 -0400

    search

--- src/sample/test/test_search.py
+++ src/sample/test/test_search.py
@@ -42,7 +42,7 @@ def main():
                                                                                   'checked, in degrees')
     parser.add_argument('--min_angle', type=int, default=0, help='min angle of rotation in degrees')
     parser.add_argument('--max_angle', type=int, default=360, help='min angle of rotation in degrees')
-    parser.add_argument('--conformer_n', type=int, default=3, help='number of conformers processed in each job')
+    parser.add_argument('--conformer_n', type=int, default=10, help='number of conformers processed in each job')
     parser.add_argument('--rmsd_cutoff', type=int, default=2.5,
                         help='rmsd accuracy cutoff between predicted ligand pose '
                              'and true ligand pose')
@@ -82,80 +82,81 @@ def main():
     conformer_index = random.choice([i for i in range(len(grouped_conformer_indices))])
 
     pose_file = os.path.join(pose_path, 'exhaustive_search_poses_{}_{}.csv'.format(grid_index, conformer_index))
+    print(pose_file)
 
-    if not os.path.exists(pose_file):
-        cmd = 'sbatch -p rondror -t 0:20:00 -o {} --wrap="$SCHRODINGER/run python3 search.py group {} {} {} ' \
-              '--rotation_search_step_size {} --grid_size {} --grid_n {} --num_conformers {} ' \
-              '--conformer_n {} --grid_index {} --conformer_index {} --protein {} --target {} --start {}"'
-        out_file_name = 'search_{}_{}_{}_{}_{}.out'.format(protein, target, start, grid_index, conformer_index)
-        os.system(
-            cmd.format(os.path.join(args.run_path, out_file_name), args.docked_prot_file, args.run_path,
-                       args.raw_root, args.rotation_search_step_size, args.grid_size, args.grid_n,
-                       args.num_conformers, args.conformer_n, grid_index, conformer_index, protein, target, start))
-    else:
-        df = pd.read_csv(pose_file)
-        tolerable_indices = [i for i in df.index]
-        pose_index = random.choice(tolerable_indices)
-
-        conformer_index = df.loc[[pose_index]]['conformer_index'].iloc[0]
-        c = conformers[conformer_index]
-        old_coords = c.getXYZ(copy=True)
-        grid_loc_x = df.loc[[pose_index]]['grid_loc_x'].iloc[0]
-        grid_loc_y = df.loc[[pose_index]]['grid_loc_y'].iloc[0]
-        grid_loc_z = df.loc[[pose_index]]['grid_loc_z'].iloc[0]
-        translate_structure(c, grid_loc_x, grid_loc_y, grid_loc_z)
-        conformer_center = list(get_centroid(c))
-        coords = c.getXYZ(copy=True)
-        rot_x = df.loc[[pose_index]]['rot_x'].iloc[0]
-        rot_y = df.loc[[pose_index]]['rot_y'].iloc[0]
-        rot_z = df.loc[[pose_index]]['rot_z'].iloc[0]
-
-        displacement_vector = get_coords_array_from_list(conformer_center)
-        to_origin_matrix = get_translation_matrix(-1 * displacement_vector)
-        from_origin_matrix = get_translation_matrix(displacement_vector)
-        rot_matrix_x = get_rotation_matrix(X_AXIS, math.radians(rot_x))
-        rot_matrix_y = get_rotation_matrix(Y_AXIS, math.radians(rot_y))
-        rot_matrix_z = get_rotation_matrix(Z_AXIS, math.radians(rot_z))
-        new_coords = rotate_structure(coords, from_origin_matrix, to_origin_matrix, rot_matrix_x,
-                                      rot_matrix_y, rot_matrix_z)
-
-        # for clash features dictionary
-        c.setXYZ(new_coords)
-
-        df_start_clash = df.loc[[pose_index]]['start_clash'].iloc[0]
-        df_target_clash = df.loc[[pose_index]]['target_clash'].iloc[0]
-        df_rmsd = df.loc[[pose_index]]['rmsd'].iloc[0]
-
-        # prots
-        start_prot_file = os.path.join(pair_path, '{}_prot.mae'.format(start))
-        start_prot = list(structure.StructureReader(start_prot_file))[0]
-
-        target_prot_file = os.path.join(pair_path, '{}_prot.mae'.format(target))
-        target_prot = list(structure.StructureReader(target_prot_file))[0]
-
-        # clash preprocessing
-        start_prot_grid, start_origin = get_grid(start_prot)
-        target_prot_grid, target_origin = get_grid(target_prot)
-
-        c_indices = [a.index for a in c.atom if a.element != 'H']
-
-        # ground truth lig
-        target_lig_file = os.path.join(pair_path, 'ligand_poses', '{}_lig0.mae'.format(target))
-        target_lig = list(structure.StructureReader(target_lig_file))[0]
-
-        # get non hydrogen atom indices for rmsd
-        target_lig_indices = [a.index for a in target_lig.atom if a.element != 'H']
-
-        rmsd_val = rmsd.calculate_in_place_rmsd(c, c_indices, target_lig, target_lig_indices)
-        start_clash = get_clash(c, start_prot_grid, start_origin)
-        target_clash = get_clash(c, target_prot_grid, target_origin)
-
-        assert(df_start_clash == start_clash)
-        assert (df_target_clash == target_clash)
-        assert (df_rmsd - rmsd_val < EPS)
-        print('All correct!')
-
-        c.setXYZ(old_coords)
+    # if not os.path.exists(pose_file):
+    #     cmd = 'sbatch -p rondror -t 0:20:00 -o {} --wrap="$SCHRODINGER/run python3 search.py group {} {} {} ' \
+    #           '--rotation_search_step_size {} --grid_size {} --grid_n {} --num_conformers {} ' \
+    #           '--conformer_n {} --grid_index {} --conformer_index {} --protein {} --target {} --start {}"'
+    #     out_file_name = 'search_{}_{}_{}_{}_{}.out'.format(protein, target, start, grid_index, conformer_index)
+    #     os.system(
+    #         cmd.format(os.path.join(args.run_path, out_file_name), args.docked_prot_file, args.run_path,
+    #                    args.raw_root, args.rotation_search_step_size, args.grid_size, args.grid_n,
+    #                    args.num_conformers, args.conformer_n, grid_index, conformer_index, protein, target, start))
+    # else:
+    #     df = pd.read_csv(pose_file)
+    #     tolerable_indices = [i for i in df.index]
+    #     pose_index = random.choice(tolerable_indices)
+    #
+    #     conformer_index = df.loc[[pose_index]]['conformer_index'].iloc[0]
+    #     c = conformers[conformer_index]
+    #     old_coords = c.getXYZ(copy=True)
+    #     grid_loc_x = df.loc[[pose_index]]['grid_loc_x'].iloc[0]
+    #     grid_loc_y = df.loc[[pose_index]]['grid_loc_y'].iloc[0]
+    #     grid_loc_z = df.loc[[pose_index]]['grid_loc_z'].iloc[0]
+    #     translate_structure(c, grid_loc_x, grid_loc_y, grid_loc_z)
+    #     conformer_center = list(get_centroid(c))
+    #     coords = c.getXYZ(copy=True)
+    #     rot_x = df.loc[[pose_index]]['rot_x'].iloc[0]
+    #     rot_y = df.loc[[pose_index]]['rot_y'].iloc[0]
+    #     rot_z = df.loc[[pose_index]]['rot_z'].iloc[0]
+    #
+    #     displacement_vector = get_coords_array_from_list(conformer_center)
+    #     to_origin_matrix = get_translation_matrix(-1 * displacement_vector)
+    #     from_origin_matrix = get_translation_matrix(displacement_vector)
+    #     rot_matrix_x = get_rotation_matrix(X_AXIS, math.radians(rot_x))
+    #     rot_matrix_y = get_rotation_matrix(Y_AXIS, math.radians(rot_y))
+    #     rot_matrix_z = get_rotation_matrix(Z_AXIS, math.radians(rot_z))
+    #     new_coords = rotate_structure(coords, from_origin_matrix, to_origin_matrix, rot_matrix_x,
+    #                                   rot_matrix_y, rot_matrix_z)
+    #
+    #     # for clash features dictionary
+    #     c.setXYZ(new_coords)
+    #
+    #     df_start_clash = df.loc[[pose_index]]['start_clash'].iloc[0]
+    #     df_target_clash = df.loc[[pose_index]]['target_clash'].iloc[0]
+    #     df_rmsd = df.loc[[pose_index]]['rmsd'].iloc[0]
+    #
+    #     # prots
+    #     start_prot_file = os.path.join(pair_path, '{}_prot.mae'.format(start))
+    #     start_prot = list(structure.StructureReader(start_prot_file))[0]
+    #
+    #     target_prot_file = os.path.join(pair_path, '{}_prot.mae'.format(target))
+    #     target_prot = list(structure.StructureReader(target_prot_file))[0]
+    #
+    #     # clash preprocessing
+    #     start_prot_grid, start_origin = get_grid(start_prot)
+    #     target_prot_grid, target_origin = get_grid(target_prot)
+    #
+    #     c_indices = [a.index for a in c.atom if a.element != 'H']
+    #
+    #     # ground truth lig
+    #     target_lig_file = os.path.join(pair_path, 'ligand_poses', '{}_lig0.mae'.format(target))
+    #     target_lig = list(structure.StructureReader(target_lig_file))[0]
+    #
+    #     # get non hydrogen atom indices for rmsd
+    #     target_lig_indices = [a.index for a in target_lig.atom if a.element != 'H']
+    #
+    #     rmsd_val = rmsd.calculate_in_place_rmsd(c, c_indices, target_lig, target_lig_indices)
+    #     start_clash = get_clash(c, start_prot_grid, start_origin)
+    #     target_clash = get_clash(c, target_prot_grid, target_origin)
+    #
+    #     assert(df_start_clash == start_clash)
+    #     assert (df_target_clash == target_clash)
+    #     assert (df_rmsd - rmsd_val < EPS)
+    #     print('All correct!')
+    #
+    #     c.setXYZ(old_coords)
 
 
 
