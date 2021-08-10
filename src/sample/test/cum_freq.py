6b015136053fc1a306e1084ba8fcc48d3e9e0ca1
sidhikabalachandar <sidhikab@stanford.edu>
2021-08-10 12:10:58 -0400

    cum_freq

--- src/sample/test/cum_freq.py
+++ src/sample/test/cum_freq.py
@@ -10,22 +10,45 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 import sys
 sys.path.insert(1, '../util')
 from util import *
 from prot_util import *
 
 
-def bar_graph(glide_ls, score_no_vdw_ls, pose_ls, args):
+def bar_graph(glide_ls, score_no_vdw_ls, random_ls, pose_ls, args):
     fig, ax = plt.subplots()
     plt.plot(pose_ls, glide_ls, label='Glide')
     plt.plot(pose_ls, score_no_vdw_ls, label='Score no vdw')
+    plt.plot(pose_ls, random_ls, label='Random')
 
     ax.legend()
     ax.set_xlabel('Pose Cutoff')
     ax.set_ylabel('Min RMSD')
     plt.title('Min RMSD Pose for {}_{}-to-{}'.format(args.protein, args.target, args.start))
     plt.savefig(os.path.join(args.out_dir, '{}_{}_{}.png'.format(args.protein, args.target, args.start)))
+
+
+def get_random_data(data, args):
+    random_datas = []
+    for _ in range(100):
+        random_data = random.shuffle(data)
+        random_data = [(i, random_data[i][0], random_data[i][1]) for i in range(len(random_data))]
+        rev_random_data = sorted(random_data, key=lambda x: x[1], reverse=True)
+        sorted_random = sorted(rev_random_data, key=lambda x: x[0])
+        random_ls = []
+
+        for i in range(1, args.num_poses_graphed):
+            random_ls.append(min(sorted_random[:i], key=lambda x: x[1])[1])
+
+        random_datas.append(random_ls)
+
+    random_datas_np = np.array(random_datas)
+    random_datas_np = np.mean(random_datas_np, axis=0)
+    print(random_datas_np.shape)
+    return random_datas_np
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -81,6 +104,7 @@ def main():
 
     glide_data = [(glide_scores[i], rmsds[i], names[i]) for i in range(len(rmsds))]
     score_no_vdw_data = [(score_no_vdws[i], rmsds[i], names[i]) for i in range(len(rmsds))]
+    data = [(rmsds[i], names[i]) for i in range(len(rmsds))]
 
     # sort data in reverse rmsd order (make sure that we choose worst in tie breakers)
     rev_glide_data = sorted(glide_data, key=lambda x: x[1], reverse=True)
@@ -99,7 +123,10 @@ def main():
         print("score_no_vdw:", min(sorted_score_no_vdw[:i], key=lambda x: x[1]))
         glide_ls.append(min(sorted_glide[:i], key=lambda x: x[1])[1])
         score_no_vdw_ls.append(min(sorted_score_no_vdw[:i], key=lambda x: x[1])[1])
-    bar_graph(glide_ls, score_no_vdw_ls, pose_ls, args)
+
+    random_ls = get_random_data(data, args)
+
+    bar_graph(glide_ls, score_no_vdw_ls, random_ls, pose_ls, args)
 
 
 if __name__=="__main__":
