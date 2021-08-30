5f1eb374dc2f77ae98e550bdd7a87134cb506044
sidhikabalachandar <sidhikab@stanford.edu>
2021-08-30 12:46:09 -0400

    get_pockets

--- src/score/get_ligands.py
+++ src/score/get_ligands.py
@@ -2,7 +2,7 @@
 The purpose of this code is to get the physics scores and the rmsds
 
 It can be run on sherlock using
-$ $SCHRODINGER/run python3 get_ligands.py run /home/users/sidhikab/lig_clash_score/src/sample/test/run /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d --max_num_concurrent_jobs 1
+$ $SCHRODINGER/run python3 get_ligands.py group /home/users/sidhikab/lig_clash_score/src/sample/test/run /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d --protein P02829 --target 2weq --start 2yge
 """
 
 import argparse
