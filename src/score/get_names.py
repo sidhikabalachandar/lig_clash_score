f1d3b3d57cb7b156ab72b26224160526d2952ca7
sidhikabalachandar <sidhikab@stanford.edu>
2021-08-30 13:24:46 -0400

    get_names

--- src/score/get_names.py
+++ src/score/get_names.py
@@ -2,7 +2,7 @@
 The purpose of this code is to get the physics scores and the rmsds
 
 It can be run on sherlock using
-$ $SCHRODINGER/run python3 get_ligands.py all /home/users/sidhikab/lig_clash_score/src/score/run /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d --protein P02829 --target 2weq --start 2yge
+$ $SCHRODINGER/run python3 get_names.py /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d
 """
 
 import argparse
@@ -25,17 +25,7 @@ Z_AXIS = [0.0, 0.0, 1.0]  # z-axis unit vector
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('task', type=str, help='either run, check, or delete_json')
-    parser.add_argument('run_path', type=str, help='directory where script and output files will be written')
     parser.add_argument('root', type=str, help='directory where raw data will be placed')
-    parser.add_argument('--protein', type=str, default='', help='protein name')
-    parser.add_argument('--target', type=str, default='', help='target ligand name')
-    parser.add_argument('--start', type=str, default='', help='start ligand name')
-    parser.add_argument('--n', type=int, default=35, help='number of protein, target, start groups processed in '
-                                                         'group task')
-    parser.add_argument('--max_num_concurrent_jobs', type=int, default=200, help='maximum number of concurrent jobs '
-                                                                                 'that can be run on slurm at one time')
-    parser.add_argument('--residue_cutoff', type=int, default=3, help='name of pose group subdir')
     args = parser.parse_args()
 
     raw_root = os.path.join(args.root, 'raw')
@@ -46,28 +36,17 @@ def main():
     if not os.path.exists(save_directory):
         os.mkdir(save_directory)
 
-    if not os.path.exists(args.run_path):
-        os.mkdir(args.run_path)
-
-    if args.task == 'all':
-        for protein, target, start in [('P02829', '2weq', '2yge'), ('P00797', '3own', '3d91'),
-                                       ('C8B467', '5ult', '5uov'),
-                                       ('P03368', '1gno', '1zp8'), ('P02829', '2fxs', '2weq'),
-                                       ('P11838', '3wz6', '1gvx'),
-                                       ('P00523', '4ybk', '2oiq'), ('P00519', '4twp', '5hu9'),
-                                       ('P0DOX7', '6msy', '6mub')]:
-            cmd = 'sbatch -p rondror -t 1:00:00 -o {} --wrap="$SCHRODINGER/run python3 get_ligands.py group {} {} ' \
-                  '--protein {} --target {} --start {}"'
-            os.system(
-                cmd.format(os.path.join(args.run_path, 'ligands_{}_{}_{}.out'.format(protein, target, start)),
-                           args.run_path, args.root, protein, target, start))
-
-    if args.task == 'group':
-        pair = '{}-to-{}'.format(args.target, args.start)
-        protein_path = os.path.join(raw_root, args.protein)
+    for protein, target, start in [('P02829', '2weq', '2yge'), ('P00797', '3own', '3d91'),
+                                   ('C8B467', '5ult', '5uov'),
+                                   ('P03368', '1gno', '1zp8'), ('P02829', '2fxs', '2weq'),
+                                   ('P11838', '3wz6', '1gvx'),
+                                   ('P00523', '4ybk', '2oiq'), ('P00519', '4twp', '5hu9'),
+                                   ('P0DOX7', '6msy', '6mub')]:
+        pair = '{}-to-{}'.format(target, start)
+        protein_path = os.path.join(raw_root, protein)
         pair_path = os.path.join(protein_path, pair)
 
-        grid_size = get_grid_size(pair_path, args.target, args.start)
+        grid_size = get_grid_size(pair_path, target, start)
         group_name = 'test_grid_{}_2_rotation_0_360_20_rmsd_2.5'.format(grid_size)
         pose_path = os.path.join(pair_path, group_name)
         file = os.path.join(pose_path, 'poses_after_advanced_filter.csv')
@@ -79,14 +58,14 @@ def main():
 
         for name in names:
             start_time = time.time()
-            pose_name = '{}_{}_{}'.format(args.protein, pair, name)
+            pose_name = '{}_{}_{}'.format(protein, pair, name)
             rmsd = df[df['name'] == name]['rmsd'].iloc[0]
             data['name'].append(pose_name)
             data['label'].append(rmsd)
-            data['pocket_file'].append('{}_{}_pocket.mae'.format(args.protein, pair))
-            data['file'].append('{}_{}_ligs.mae'.format(args.protein, pair))
+            data['pocket_file'].append('{}_{}_pocket.mae'.format(protein, pair))
+            data['file'].append('{}_{}_ligs.mae'.format(protein, pair))
             print(time.time() - start_time)
-            return 
+            return
 
         df = pd.DataFrame.from_dict(data)
         df.to_csv(os.path.join(save_directory, 'names.csv'), index=False)
