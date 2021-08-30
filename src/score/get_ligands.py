59c54acf41d886cbe503323f3302145f35900f4f
sidhikabalachandar <sidhikab@stanford.edu>
2021-08-30 12:44:54 -0400

    get_pockets

--- src/score/get_ligands.py
+++ src/score/get_ligands.py
@@ -2,14 +2,13 @@
 The purpose of this code is to get the physics scores and the rmsds
 
 It can be run on sherlock using
-$ $SCHRODINGER/run python3 get_ligands.py run /home/users/sidhikab/lig_clash_score/src/sample/test/run /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d/raw --max_num_concurrent_jobs 1
+$ $SCHRODINGER/run python3 get_ligands.py run /home/users/sidhikab/lig_clash_score/src/sample/test/run /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d --max_num_concurrent_jobs 1
 """
 
 import argparse
 import os
 import schrodinger.structure as structure
 import pandas as pd
-import time
 
 import sys
 sys.path.insert(1, '../sample/util')
@@ -28,7 +27,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('task', type=str, help='either run, check, or delete_json')
     parser.add_argument('run_path', type=str, help='directory where script and output files will be written')
-    parser.add_argument('raw_root', type=str, help='directory where raw data will be placed')
+    parser.add_argument('root', type=str, help='directory where raw data will be placed')
     parser.add_argument('--protein', type=str, default='', help='protein name')
     parser.add_argument('--target', type=str, default='', help='target ligand name')
     parser.add_argument('--start', type=str, default='', help='start ligand name')
@@ -38,6 +37,14 @@ def main():
                                                                                  'that can be run on slurm at one time')
     parser.add_argument('--residue_cutoff', type=int, default=3, help='name of pose group subdir')
     args = parser.parse_args()
+
+    raw_root = os.path.join(args.root, 'raw')
+    save_directory = os.path.join(args.root, 'ml_score')
+    if not os.path.exists(save_directory):
+        os.mkdir(save_directory)
+    save_directory = os.path.join(save_directory, 'data')
+    if not os.path.exists(save_directory):
+        os.mkdir(save_directory)
 
     if not os.path.exists(args.run_path):
         os.mkdir(args.run_path)
@@ -49,10 +56,15 @@ def main():
                                        ('P11838', '3wz6', '1gvx'),
                                        ('P00523', '4ybk', '2oiq'), ('P00519', '4twp', '5hu9'),
                                        ('P0DOX7', '6msy', '6mub')]:
+            cmd = 'sbatch -p owners -t 1:00:00 -o {} --wrap=$SCHRODINGER/run python3 get_ligands.py group {} {} ' \
+                  '--protein {} --target {} --start {}'
+            os.system(
+                cmd.format(os.path.join(args.run_path, 'ligands_{}_{}_{}.out'.format(protein, target, start)),
+                           args.run_path, args.root, protein, target, start))
 
     if args.task == 'group':
         pair = '{}-to-{}'.format(args.target, args.start)
-        protein_path = os.path.join(args.raw_root, args.protein)
+        protein_path = os.path.join(raw_root, args.protein)
         pair_path = os.path.join(protein_path, pair)
 
         grid_size = get_grid_size(pair_path, args.target, args.start)
@@ -66,32 +78,27 @@ def main():
         conformer_file = os.path.join(pair_path, "aligned_to_start_with_hydrogen_conformers.mae")
         conformers = list(structure.StructureReader(conformer_file))
 
-        for name in names:
-            conformer_index = df[df['name'] == name]['conformer_index'].iloc[0]
-            c = conformers[conformer_index]
-            old_coords = c.getXYZ(copy=True)
-            grid_loc_x = df[df['name'] == name]['grid_loc_x'].iloc[0]
-            grid_loc_y = df[df['name'] == name]['grid_loc_y'].iloc[0]
-            grid_loc_z = df[df['name'] == name]['grid_loc_z'].iloc[0]
-            rot_x = df[df['name'] == name]['rot_x'].iloc[0]
-            rot_y = df[df['name'] == name]['rot_y'].iloc[0]
-            rot_z = df[df['name'] == name]['rot_z'].iloc[0]
+        ligand_file = os.path.join(save_directory, '{}_{}_ligs.mae'.format(args.protein, pair))
+        with structure.StructureWriter(ligand_file) as filtered:
+            for name in names:
+                pose_name = '{}_{}_{}'.format(args.protein, pair, name)
+                conformer_index = df[df['name'] == name]['conformer_index'].iloc[0]
+                c = conformers[conformer_index]
+                old_coords = c.getXYZ(copy=True)
+                grid_loc_x = df[df['name'] == name]['grid_loc_x'].iloc[0]
+                grid_loc_y = df[df['name'] == name]['grid_loc_y'].iloc[0]
+                grid_loc_z = df[df['name'] == name]['grid_loc_z'].iloc[0]
+                rot_x = df[df['name'] == name]['rot_x'].iloc[0]
+                rot_y = df[df['name'] == name]['rot_y'].iloc[0]
+                rot_z = df[df['name'] == name]['rot_z'].iloc[0]
 
-            new_coords = create_pose(c, grid_loc_x, grid_loc_y, grid_loc_z, rot_x, rot_y, rot_z)
+                new_coords = create_pose(c, grid_loc_x, grid_loc_y, grid_loc_z, rot_x, rot_y, rot_z)
 
-            # for clash features dictionary
-            c.setXYZ(new_coords)
-            c.title = name
-            save_path = os.path.join(pose_path, 'ml_score')
-            if not os.path.exists(save_path):
-                os.mkdir(save_path)
-            save_path = os.path.join(save_path, 'data')
-            if not os.path.exists(save_path):
-                os.mkdir(save_path)
-            ligand_file = os.path.join(save_path, '{}_lig.mae'.format(name))
-            with structure.StructureWriter(ligand_file) as filtered:
+                # for clash features dictionary
+                c.setXYZ(new_coords)
+                c.title = pose_name
                 filtered.append(c)
-            c.setXYZ(old_coords)
+                c.setXYZ(old_coords)
 
 
 if __name__=="__main__":
