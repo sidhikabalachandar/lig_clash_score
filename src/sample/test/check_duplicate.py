5990ca5d061f053e77addcab3620020960f5ede0
sidhikabalachandar <sidhikab@stanford.edu>
2021-09-03 15:47:27 -0400

    python score only

--- src/sample/test/check_duplicate.py
+++ src/sample/test/check_duplicate.py
@@ -27,10 +27,7 @@ from read_vdw_params import *
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('task', type=str, help='either align or search')
-    parser.add_argument('run_path', type=str, help='directory where script and output files will be written')
     parser.add_argument('raw_root', type=str, help='directory where raw data will be placed')
-    parser.add_argument('vdw_param_file', type=str, help='directory where raw data will be placed')
     parser.add_argument('--protein', type=str, default='', help='protein name')
     parser.add_argument('--target', type=str, default='', help='target ligand name')
     parser.add_argument('--start', type=str, default='', help='start ligand name')
@@ -52,50 +49,45 @@ def main():
 
     random.seed(0)
 
-    if not os.path.exists(args.run_path):
-        os.mkdir(args.run_path)
+    for protein, target, start in [('P03368', '1gno', '1zp8'), ('P02829', '2fxs', '2weq'),
+                                   ('P11838', '3wz6', '1gvx'), ('P00523', '4ybk', '2oiq'),
+                                   ('P00519', '4twp', '5hu9'), ('P0DOX7', '6msy', '6mub')]:
+        pair = '{}-to-{}'.format(target, start)
+        protein_path = os.path.join(args.raw_root, protein)
+        pair_path = os.path.join(protein_path, pair)
 
-    if args.task == 'all':
-        counter = 0
-        for protein, target, start in [('P03368', '1gno', '1zp8'), ('P02829', '2fxs', '2weq'),
-                                       ('P11838', '3wz6', '1gvx'), ('P00523', '4ybk', '2oiq'),
-                                       ('P00519', '4twp', '5hu9'), ('P0DOX7', '6msy', '6mub')]:
-            pair = '{}-to-{}'.format(target, start)
-            protein_path = os.path.join(args.raw_root, protein)
-            pair_path = os.path.join(protein_path, pair)
+        grid_size = get_grid_size(pair_path, target, start)
+        group_name = 'test_grid_{}_2_rotation_0_360_20_rmsd_2.5'.format(grid_size)
+        pose_path = os.path.join(pair_path, group_name)
 
-            grid_size = get_grid_size(pair_path, target, start)
-            group_name = 'test_grid_{}_2_rotation_0_360_20_rmsd_2.5'.format(grid_size)
-            pose_path = os.path.join(pair_path, group_name)
+        clash_path = os.path.join(pose_path, 'clash_data')
+        dfs = []
+        for file in os.listdir(clash_path):
+            prefix = 'pose_pred_data'
+            if file[:len(prefix)] == prefix:
+                df = pd.read_csv(os.path.join(clash_path, file))
+                filter_df = df[df['pred_num_intolerable'] < args.residue_cutoff]
+                dfs.append(filter_df)
 
-            clash_path = os.path.join(pose_path, 'clash_data')
-            dfs = []
-            for file in os.listdir(clash_path):
-                prefix = 'pose_pred_data'
-                if file[:len(prefix)] == prefix:
-                    df = pd.read_csv(os.path.join(clash_path, file))
-                    filter_df = df[df['pred_num_intolerable'] < args.residue_cutoff]
-                    dfs.append(filter_df)
+        df = pd.concat(dfs)
+        correct_df = df[df['rmsd'] < args.rmsd_cutoff]
+        correct_names = correct_df['name'].to_list()
+        random.shuffle(correct_names)
+        correct_names = correct_names[:args.max_num_correct]
+        incorrect_df = df[df['rmsd'] >= args.rmsd_cutoff]
+        incorrect_names = incorrect_df['name'].to_list()
+        random.shuffle(incorrect_names)
+        incorrect_names = incorrect_names[:args.max_num_poses_considered - len(correct_names)]
+        names = correct_names + incorrect_names
 
-            df = pd.concat(dfs)
-            correct_df = df[df['rmsd'] < args.rmsd_cutoff]
-            correct_names = correct_df['name'].to_list()
-            random.shuffle(correct_names)
-            correct_names = correct_names[:args.max_num_correct]
-            incorrect_df = df[df['rmsd'] >= args.rmsd_cutoff]
-            incorrect_names = incorrect_df['name'].to_list()
-            random.shuffle(incorrect_names)
-            incorrect_names = incorrect_names[:args.max_num_poses_considered - len(correct_names)]
-            names = correct_names + incorrect_names
+        print(protein, target, start)
+        distinct = []
 
-            print(protein, target, start)
-            distinct = []
+        for name in names:
+            if name not in distinct:
+                distinct.append(name)
 
-            for name in names:
-                if name not in distinct:
-                    distinct.append(name)
-
-            assert(len(names) != len(distinct))
+        assert(len(names) != len(distinct))
 
 
 
