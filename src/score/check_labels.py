b1976a11004efb66966293e2d9a2eb514f9b3134
sidhikabalachandar <sidhikab@stanford.edu>
2021-09-03 17:58:24 -0400

    check_labels

--- src/score/check_labels.py
+++ src/score/check_labels.py
@@ -2,45 +2,15 @@
 The purpose of this code is to get the physics scores and the rmsds
 
 It can be run on sherlock using
-$ $SCHRODINGER/run python3 get_names.py /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d
+$ $SCHRODINGER/run python3 check_labels.py /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d
 """
 
-import argparse
-import os
 import pandas as pd
-import time
-
-import sys
-sys.path.insert(1, '../sample/util')
-from util import *
-from prot_util import *
-from schrod_replacement_util import *
-from lig_util import *
-from prepare_pockets import *
-
-X_AXIS = [1.0, 0.0, 0.0]  # x-axis unit vector
-Y_AXIS = [0.0, 1.0, 0.0]  # y-axis unit vector
-Z_AXIS = [0.0, 0.0, 1.0]  # z-axis unit vector
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('root', type=str, help='directory where raw data will be placed')
-    parser.add_argument('--rmsd_cutoff', type=float, default=2.5, help='name of pose group subdir')
-    args = parser.parse_args()
-
-    raw_root = os.path.join(args.root, 'raw')
-    save_directory = os.path.join(args.root, 'ml_score')
-    if not os.path.exists(save_directory):
-        os.mkdir(save_directory)
-    save_directory = os.path.join(save_directory, 'data')
-    if not os.path.exists(save_directory):
-        os.mkdir(save_directory)
-
-    df = pd.read_csv(os.path.join(save_directory, 'names.csv'))
-
+    df = pd.read_csv('/oak/stanford/groups/rondror/projects/combind/flexibility/atom3d/names.csv')
     print(df.label.unique())
-
 
 
 if __name__=="__main__":
