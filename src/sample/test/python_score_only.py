9d44390bd7d1c60d2a39a2643f80829196b044ce
sidhikabalachandar <sidhikab@stanford.edu>
2021-09-03 15:31:35 -0400

    python score only

--- src/sample/test/python_score_only.py
+++ src/sample/test/python_score_only.py
@@ -2,7 +2,7 @@
 The purpose of this code is to create the cumulative frequency and bar graphs
 
 It can be run on sherlock using
-$ $SCHRODINGER/run python3 python_score_only.py group /home/users/sidhikab/lig_clash_score/src/sample/test/run /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d/raw /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d/vdw_AMBER_parm99.defn --protein P00523 --target 4ybk --start 2oiq --index 10
+$ $SCHRODINGER/run python3 python_score_only.py group /home/users/sidhikab/lig_clash_score/src/sample/test/run /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d/raw /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d/vdw_AMBER_parm99.defn --protein P03368 --target 1gno --start 1zp8 --index 10
 """
 
 import argparse
@@ -145,7 +145,11 @@ def main():
         names = []
 
         print(len(grouped_names[args.index]))
-        print(len(set(grouped_names[args.index])))
+        distinct = []
+        for name in grouped_names[args.index]:
+            if name not in distinct:
+                distinct.append(name)
+        print(len(distinct))
         return
 
         for name in grouped_names[args.index]:
