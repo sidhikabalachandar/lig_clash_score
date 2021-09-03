141effe42bed61086ba8f0098167f273af27c91c
sidhikabalachandar <sidhikab@stanford.edu>
2021-09-03 17:59:13 -0400

    check_labels

--- src/score/check_labels.py
+++ src/score/check_labels.py
@@ -2,14 +2,14 @@
 The purpose of this code is to get the physics scores and the rmsds
 
 It can be run on sherlock using
-$ $SCHRODINGER/run python3 check_labels.py /oak/stanford/groups/rondror/projects/combind/flexibility/atom3d
+$ $SCHRODINGER/run python3 check_labels.py
 """
 
 import pandas as pd
 
 
 def main():
-    df = pd.read_csv('/oak/stanford/groups/rondror/projects/combind/flexibility/atom3d/names.csv')
+    df = pd.read_csv('/oak/stanford/groups/rondror/projects/combind/flexibility/atom3d/ml_score/data/names.csv')
     print(df.label.unique())
 
 
