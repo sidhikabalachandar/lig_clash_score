1261e8e9b43950363942706f69c10cca9b91cf69
sidhikabalachandar <sidhikab@stanford.edu>
2021-09-22 13:12:24 -0700

    test

--- src/score/train_scoring_model.sh
+++ src/score/train_scoring_model.sh
@@ -3,7 +3,6 @@ WEIGHTS_SAVE_PATH=/oak/stanford/groups/rondror/projects/combind/flexibility/atom
 NAME='small_lig_clash_score'
 DATA_DIR=/oak/stanford/groups/rondror/projects/combind/flexibility/atom3d/ml_score/small_lmdb_data/split/data
 cd $MODEL_DIR
-#AUGMENT='AUGMENT'
 export DATA_DIR=$DATA_DIR
 export MODEL_DIR=$MODEL_DIR
 export WEIGHTS_SAVE_PATH=$WEIGHTS_SAVE_PATH
@@ -11,7 +10,4 @@ TASK='regression'
 echo $WEIGHTS_SAVE_PATH
 echo $MODEL_DIR
 
-#python train.py --project_name fragment_stitching --run_id $NAME --weights_save_path /
-#$WEIGHTS_SAVE_PATH --balance_dataset --num_workers=8 --gpus=1 --batch_size=16 --max_epochs=40 /
-# --prefix binding_affinity
 python train.py -train $DATA_DIR/train -val $DATA_DIR/val -test $DATA_DIR/test --weights_save_path $WEIGHTS_SAVE_PATH --gpus=1 --num_workers=8 --batch_size=8 --accumulate_grad_batches=2 --learning_rate=0.01 --max_epochs=40 --project_name pdbbind --run_id lig_clash_score --balance_dataset
